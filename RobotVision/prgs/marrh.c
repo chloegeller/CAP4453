dc704907f9ceb458a45b345deaa6f7bb031312e9
h3rmi0n3 <chloegeller@knights.ucf.edu>
2020-01-22 00:48:25 -0500

    Changes to be committed:

--- RobotVision/prgs/marrh.c
+++ RobotVision/prgs/marrh.c
@@ -264,6 +264,8 @@ int main(int argc, char *argv[])
     hiTSH = tsh;
     loTSH = hiTSH * 0.35;
 
+    loTSH = 0.0;
+
     printf("Percentage value: %d\n", percentage);
     printf("High treshold value: %d\n", hiTSH);
     printf("Low threshold value: %d\n", loTSH);
